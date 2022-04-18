4c987dbb9437e88f51901b12c6dd427758f42e8b
tiendung050803 <tiendung050803@gmail.com>
2022-04-18 23:19:24 +0700

    Update main.cpp

--- Project Game/main.cpp
+++ Project Game/main.cpp
@@ -5,8 +5,11 @@
 //  Created by Nguyễn Tiến Dũng on 4/18/22.
 //  Copyright © 2022 Nguyễn Tiến Dũng. All rights reserved.
 //
-
+#include <bits/stdc++.h>
 #include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_ttf.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
