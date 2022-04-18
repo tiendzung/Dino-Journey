0821713971538184ca49750c6423f8f68bd38edb
tiendung050803 <tiendung050803@gmail.com>
2022-04-18 22:53:58 +0700

    Initial Commit

---
+++ Project Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project Game
+//
+//  Created by Nguyễn Tiến Dũng on 4/18/22.
+//  Copyright © 2022 Nguyễn Tiến Dũng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
