d23c4905d410bbb3640eccafc5415a646b173704
tiendung050803 <tiendung050803@gmail.com>
2022-04-19 17:43:00 +0700

    Update main.cpp

--- Project Game/main.cpp
+++ Project Game/main.cpp
@@ -1,18 +1,85 @@
-//
-//  main.cpp
-//  Project Game
-//
-//  Created by Nguyễn Tiến Dũng on 4/18/22.
-//  Copyright © 2022 Nguyễn Tiến Dũng. All rights reserved.
-//
+//#include "stdafx.h"
 #include <bits/stdc++.h>
-#include <iostream>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_ttf.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <thread>
+#include <chrono>
+using namespace std;
+SDL_Window* g_windows = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* g_background = NULL;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+bool loadMedia()
+{
+    bool success = true;
+    g_background = IMG_Load("anhdep.jpeg");
+    if( g_background == NULL )
+    {
+        success = false;
+        cout << "Failed to load Image!!!\n";
+    }
+    
+    return success;
+}
+void close()
+{
+    SDL_FreeSurface( g_background );
+    g_background = NULL;
+    SDL_DestroyWindow( g_windows );
+    g_windows = NULL;
+    SDL_Quit();
+}
+
+bool init()
+{
+    bool success = true;
+    if( SDL_Init( SDL_INIT_VIDEO ) >= 0 )
+    {
+        g_windows = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN );
+        if( g_windows != NULL )
+        {
+            gScreenSurface = SDL_GetWindowSurface(g_windows);
+        }
+    }
+    return success;
+}
+
+void waitUntilKeyPressed()
+{
+    SDL_Event e;
+    while(true)
+    {
+        if( SDL_WaitEvent(&e)!=0 && (e.type == SDL_KEYDOWN || e.type == SDL_QUIT) ) //Xac dinh su kien ban phim
+            return;
+        SDL_Delay(100);
+    }
+}
+
+int main()
+{
+    if(init() == true)
+    {
+        if(loadMedia())
+        {
+            SDL_BlitSurface( g_background, NULL, gScreenSurface, NULL );
+            SDL_UpdateWindowSurface(g_windows);
+            //                        waitUntilKeyPressed();
+            bool is_running = true;
+            SDL_Event event;
+            while (is_running) {
+                while (SDL_PollEvent(&event)) {
+                    if (event.type == SDL_QUIT) {
+                        is_running = false;
+                    }
+                }
+                //                                SDL_Delay(16);
+            }
+            //            this_thread::sleep_for(chrono::milliseconds(2000));
+        }
+    }
+    
+    close();
     return 0;
 }
